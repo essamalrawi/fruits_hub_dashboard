1c3b77b8358e9c314e8f8e4e3c07659ae61732e1
essamalrawi <essamhamdy198@gmail.com>
2025-01-24 00:25:44 +0200

    add firebase

---
+++ lib/core/helper_functions/on_generate_routes.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import '../../feature/dashbaord/views/dashboard_view.dart';
+
+Route<dynamic> onGenerateRoute(RouteSettings settings) {
+  switch (settings.name) {
+    case DashboardView.routeName:
+      return MaterialPageRoute(builder: (context) => const DashboardView());
+    default:
+      return MaterialPageRoute(builder: (context) => const Scaffold());
+  }
+}
